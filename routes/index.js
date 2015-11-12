9dd286eb581e57bc3290b78aa557de35a27da8c2
Stephen Florian <steezeven@gmail.com>
2015-11-12 13:58:37 -0700

    added basic post route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../lib/praise.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/memories', function(rew, res){
+  db.insert('memories', {
+    old_days: req.body.data.attributes.old_days, 
+    these_days: req.body.data.attributes.these_days, 
+    year: req.body.data.attributes.year
+  })
+  res.redirect('/')
+})
 
 module.exports = router;
